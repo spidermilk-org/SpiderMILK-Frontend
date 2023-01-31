78e07b74970f8e0652ecf935b2d8c2a10bd29a21
PhilVoel <philipp123.voeller@gmail.com>
2023-01-31 21:53:04 +0100

    Added rudimentary JWT verification

--- game.js
+++ game.js
@@ -3,6 +3,27 @@ const log = document.getElementById("log");
 let inputLog = [];
 let inputLogPos = 0;
 
+const jwt = getJWT();
+if(jwt == null || jwtExpired(jwt))
+    window.location.href = "/login.php";
+
+function getJWT() {
+    const cookieArray = document.cookie.split(';');
+    for(let i = 0; i < cookieArray.length; i++) {
+        const cookie = cookieArray[i].trim();
+        if(cookie.startsWith('sm_jwt_login='))
+            return cookie.substring(13);
+    }
+    return null;
+}
+
+function jwtExpired(jwt) {
+    const payload = JSON.parse(atob(jwt.split('.')[1]));
+    const now = new Date().getTime() / 1000;
+    return now > payload.exp;
+}
+
+
 if(sessionStorage.getItem("log") != null && sessionStorage.getItem("log").length > 0) {
 	overrideLog(sessionStorage.getItem("log"));
 }
