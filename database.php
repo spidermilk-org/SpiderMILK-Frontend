3c199a6ea1938cc58e0a0480246b58fae6769e05
PhilVoel <philipp123.voeller@gmail.com>
2023-02-04 02:14:09 +0100

    Moved loading of composer plugins into login.php (and later register.php)

--- database.php
+++ database.php
@@ -1,7 +1,4 @@
 <?php
-require_once __DIR__ . '/vendor/autoload.php';
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-$dotenv->load();
 
 //TODO #1 use official SurrealDB library instead of curl when it comes out
 
